44712e3471f1d27b0bbfd9e4ad3ff0c0e8eae147
hoangdinhtrong <info.hoangdinhtrong.hy@gmail.com>
2023-04-04 15:45:14 +0700

    example about connect to server

--- LearnAngular/src/app/app.component.ts
+++ LearnAngular/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpServerService } from './Services/http-server.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'LearnAngular';
+  constructor(private httpServerService: HttpServerService) {}
+
+  public ngOnInit(): void {
+    this.httpServerService.getRandomUsers(10).subscribe((data) => {
+      console.log('getRandomUsers', data.results);
+    });
+
+    const payload = { body: 'Đây là message số 4', postId: 1 };
+    this.httpServerService.postComment(payload).subscribe((data) => {
+      console.log('postComment', data);
+    });
+
+    this.httpServerService.getComments().subscribe((data) => {
+      console.log('getComments', data);
+    });
+  }
 }
