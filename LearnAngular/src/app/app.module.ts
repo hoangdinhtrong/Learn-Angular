9176a0a8b66725e7b7f400ad0eaed59d3e5ac21c
hoangdinhtrong <info.hoangdinhtrong.hy@gmail.com>
2023-04-04 11:00:20 +0700

    example about routing and ngswitch

--- LearnAngular/src/app/app.module.ts
+++ LearnAngular/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotPoundComponent } from './Components/Common/page-not-pound/page-not-pound.component';
+import { HomePageComponent } from './Components/home-page/home-page.component';
+import { AboutPageComponent } from './Components/about-page/about-page.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotPoundComponent,
+    HomePageComponent,
+    AboutPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
